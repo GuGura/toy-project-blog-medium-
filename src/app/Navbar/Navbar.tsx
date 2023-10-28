5fee419f658031b0027c6f7a343a295c6411a575
pjy <wodus331@vanillacode.net>
2023-10-28 21:25:23 +0900

    초기 폴더 구조

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -1,3 +1,18 @@
 export default async function Navbar() {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-between px-2">
+      <div className="grid gap-1">
+        <h1 className="font-heading text-3xl md:text-4xl">heading</h1>
+        <p className="text-muted-foreground text-lg">124353</p>
+      </div>
+      <div>
+        <div>fkdsndkfj</div>
+        <div>fkdsndkfj</div>
+        <div>fkdsndkfj</div>
+        <div>fkdsndkfj</div>
+        <div>fkdsndkfj</div>
+        <div>fkdsndkfj</div>
+      </div>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Button from '@/components/Button';
+import { Button } from '@/components/TextButton';
 
 export default function Home() {
   const res = 'Hello World';
   return (
     <div className={'p-3'}>
       <h1 className="sticky p-3">Home {res}</h1>
-      <Button as={'a'} href={'/'}>
-        Click me
-      </Button>
+      <Button size={'small'}>hello</Button>
     </div>
   );
 }
