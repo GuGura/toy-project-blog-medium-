5134c6025c81f18e2ead9f9222917e9eb0070f55
GuGura <wodus331@gmail.com>
2023-11-01 09:41:09 +0900

    commit

--- src/app/new-story/page.tsx
+++ src/app/new-story/page.tsx
@@ -1,6 +1,75 @@
 'use client';
 import { useState } from 'react';
 import { Plate, PlateContent } from '@udecode/plate-common';
+import {
+  createPluginFactory,
+  createPlugins,
+  ELEMENT_DEFAULT,
+  ELEMENT_PARAGRAPH,
+  getPluginType,
+  HotkeyPlugin,
+  onKeyDownToggleElement,
+  PlateRenderElementProps,
+} from '@udecode/plate';
+
+const KEY_AMAZING = 'amazing';
+const KEY_ALIGN = 'align';
+const ParagraphElement = ({
+  attributes,
+  nodeProps,
+  children,
+}: PlateRenderElementProps) => {
+  return (
+    <p {...attributes} {...nodeProps}>
+      {/* The `children` prop must always be rendered */}
+      {children}
+    </p>
+  );
+};
+const createAmazingPlugin = createPluginFactory({
+  key: KEY_AMAZING,
+  isElement: true,
+  isInline: true,
+  isVoid: true,
+});
+
+const createParagraphPlugin = createPluginFactory<HotkeyPlugin>({
+  key: ELEMENT_PARAGRAPH,
+  isElement: true,
+  handlers: {
+    // Check for the hotkey on keydown
+    onKeyDown: onKeyDownToggleElement,
+  },
+  options: {
+    // Define the hotkeys here
+    hotkey: ['mod+opt+0', 'mod+shift+0'],
+  },
+});
+
+const createAlignPlugin = createPluginFactory({
+  key: KEY_ALIGN,
+
+  // Note that we're using `then` to access to the editor
+  then: (editor) => ({
+    inject: {
+      props: {
+        nodeKey: KEY_ALIGN,
+        defaultNodeValue: 'left',
+        styleKey: 'textAlign',
+        validNodeValues: ['left', 'center', 'right', 'justify'],
+        validTypes: [getPluginType(editor, ELEMENT_DEFAULT)],
+      },
+    },
+  }),
+});
+const plugins = createPlugins(
+  [createAmazingPlugin(), createParagraphPlugin(), createAlignPlugin()],
+  {
+    components: {
+      [ELEMENT_PARAGRAPH]: ParagraphElement,
+    },
+  },
+);
 
 export default function Page() {
   const [text, setText] = useState('Fusce dapibus, tellus ac cursus commodo');
@@ -11,7 +80,7 @@ export default function Page() {
         <h1>react-medium-editor</h1>
         <h3>Html content</h3>
       </div>
-      <Plate>
+      <Plate plugins={plugins}>
         <PlateContent
           placeholder="Type..."
           className={'h-36 w-full max-w-7xl rounded border'}
